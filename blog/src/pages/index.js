2e1634d880183aa9afa13a7f5848463e0d96608e
Akash Singh <singha36@gmatc.matc.edu>
2023-02-04 22:32:09 -0600

    Completed Activity 2

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -7,16 +7,16 @@ import * as styles from "../components/index.module.css"
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <Seo title="Home"/>
+    <Seo title="Home" />
     <ul className={styles.list}>
-    {
-      data.allContentfulBlogPost.edges.map(edge => (
-        <li key={edge.node.id}>
-          <Link to={edge.node.slug}>{edge.node.title}</Link>
-        </li>
-      ))
+      {
+        data.allContentfulBlogPost.edges.map(edge => (
+          <li key={edge.node.id}>
+            <Link to={edge.node.slug}>{edge.node.title}</Link>
+          </li>
+        ))
 
-    }
+      }
     </ul>
   </Layout>
 )
@@ -31,15 +31,15 @@ export const Head = () => <Seo title="Home" />
 export default IndexPage
 
 export const query = graphql`
-{
-  allContentfulBlogPost {
-    edges {
-      node {
-        id
-        title
-        slug
+  {
+    allContentfulBlogPost{
+      edges {
+        node {
+          id
+          title
+          slug
+        }
       }
     }
   }
-}
 `
