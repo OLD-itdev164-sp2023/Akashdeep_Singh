b17d504f5bd94156e43ccc0d483b39e55eda34c2
Akash Singh <singha36@gmatc.matc.edu>
2023-02-10 20:55:02 -0600

    Preparing Blog Post

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -29,27 +29,14 @@ module.exports = {
       },
     },
     {
-        resolve: `gatsby-source-contentful`,
-        options: {
-          spaceId: `kisj29vxvjki`,
-          accessToken: `yUGdl4EhyQ6EaeFocfLI4TnkAGDLf_3uI1dllGwuGlk`
-        }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        spaceId: `${process.env.SPACE_ID}`,
+        accessToken: `${process.env.ACCESS_TOKEN}`
       },
     },
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
   ],
 }
