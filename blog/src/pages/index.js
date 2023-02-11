d1af92481b7d841774460442a0053288a217d92b
Akash Singh <singha36@gmatc.matc.edu>
2023-02-10 21:07:54 -0600

    Completed Activity 3

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,21 +1,29 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
-const IndexPage = ({ data }) => (
+const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
+    <Seo title="Home"/>
     <ul className={styles.list}>
       {
-        data.allContentfulBlogPost.edges.map(edge => (
+        DataTransfer.allContentfulBlogPost.edges.map(edge => (
           <li key={edge.node.id}>
             <Link to={edge.node.slug}>{edge.node.title}</Link>
+            <div>
+              <GatsbyImage
+                image={edge.node.heroImage.gatsbyImageData}
+              />
+            </div>
+            <div>
+              {edge.node.body.childMarkdownRemark.excerpt}
+            </div>
           </li>
         ))
-
       }
     </ul>
   </Layout>
@@ -32,14 +40,25 @@ export default IndexPage
 
 export const query = graphql`
   {
-    allContentfulBlogPost{
+    allContentfulBlogPost {
       edges {
         node {
           id
           title
           slug
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+          heroImage {
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              width: 300
+            )
+          }
         }
       }
     }
-  }
-`
+  }`
