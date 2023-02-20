c5c4d11dd2589977cef089136d2c6315ceaf398a
Akash Singh <singha36@gmatc.matc.edu>
2023-02-19 18:51:57 -0600

    Completed Activity 4

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -21,22 +21,40 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
+        resolve: `gatsby-source-filesystem`,
+        options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`, 
+        },
     },
+
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        spaceId: `${process.env.SPACE_ID}`,
-        accessToken: `${process.env.ACCESS_TOKEN}`
-      },
-    },
+    resolve: `gatsby-source-contentful`,
+    options: {
+      spaceId: `${process.env.CONTENTFUL_SPACE_ID}`,
+      accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+    }
+},
+
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-  ],
+    `gatsby-plugin-sharp`,   
+    
+    {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+    ],
 }
---
+++ blog/src/components/themes/Gray/Gray.js
@@ -0,0 +1,5 @@
+const theme = {
+    header: { backroundColor: '#555'}
+}
+
+export {theme as Gray}
