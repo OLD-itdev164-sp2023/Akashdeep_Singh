e7a9ccf7c8bd1254c11cc3ac475abc9ae1cf9571
Akash Singh <singha36@gmatc.matc.edu>
2023-04-09 17:48:43 -0500

    Completed Activity 8

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,10 +2,12 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from 'styled-components'
-export { H1 } from './Heading1'
+import { H1 } from './Heading1'
+import { IconButton } from "../Button"
+import {Section} from '../Section'
 
 const Outer = styled.header`
-  background: ${props => props.theme.header.backgroundColor};
+  background: ${({ theme }) => theme.variants.header.primary.backgroundColor};
   margin-bottom: 1.45rem;
   margin: 0 auto;
   padding: var(--space-4) var(--size-gutter);
